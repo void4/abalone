7a46127da8b3fc095523a74d9e8470a4a23fe030
void4 <arndtschnabel@freenet.de>
2019-12-28 23:59:40 +0100

    Move about page to last position

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,8 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Play</router-link> |
-      <router-link to="/about">About</router-link> |
-       <router-link to="/stats">Stats</router-link>
+      <router-link to="/stats">Stats</router-link> |
+      <router-link to="/about">About</router-link>
     </div>
     <router-view/>
   </div>
