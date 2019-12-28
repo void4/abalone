dd31f3fa3c1ce40fad1f078025f42d63f331dfc6
void4 <arndtschnabel@freenet.de>
2019-12-28 23:57:53 +0100

    Remove about page, replace it with home, make /play root /

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Play</router-link> |
       <router-link to="/about">About</router-link> |
-       <router-link to="/play">Play</router-link> |
        <router-link to="/stats">Stats</router-link>
     </div>
     <router-view/>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Home from '../components/Home.vue';
 import Play from '../components/Play.vue';
 import Stats from '../components/Stats.vue';
 
@@ -8,20 +8,12 @@ Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
+    path: '/about',
     name: 'home',
     component: Home,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
-  {
-    path: '/play',
+    path: '/',
     name: 'Play',
     component: Play,
   },
