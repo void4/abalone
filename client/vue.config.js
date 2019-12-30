2e96f0aabc0a46fdc47db06c6052a065190f011d
void4 <arndtschnabel@freenet.de>
2019-12-30 20:27:54 +0100

    Add SSL cert to client config

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,5 +2,13 @@ module.exports = {
     devServer: {
         disableHostCheck: true,
         https: true
+        host: '0.0.0.0',
+        port: 443,       // ssl defult port number
+        inline: true,
+        https: {
+              key: fs.readFileSync('/etc/letsencrypt/live/qewasd.com/privkey.pem'),
+              cert: fs.readFileSync('/etc/letsencrypt/live/qewasd.com/cert.pem'),
+              ca: fs.readFileSync('/etc/letsencrypt/live/qewasd.com/chain.pem')
+        }
     }
 }
