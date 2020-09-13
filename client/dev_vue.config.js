8c7b5dac938bef8517f324a873bfa818d1b19d4e
void4 <arndtschnabel@freenet.de>
2020-09-13 08:57:14 +0200

    Update pyabalone submodule to include .gitignore, split vue_config

---
+++ client/dev_vue.config.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+module.exports = {
+    devServer: {
+        /*
+        disableHostCheck: true,
+        https: true,
+        host: '0.0.0.0',
+        port: 443,       // ssl defult port number
+        inline: true,
+        https: {
+              key: fs.readFileSync('/etc/letsencrypt/live/qewasd.com/privkey.pem'),
+              cert: fs.readFileSync('/etc/letsencrypt/live/qewasd.com/cert.pem'),
+              ca: fs.readFileSync('/etc/letsencrypt/live/qewasd.com/chain.pem')
+        }
+        */
+    }
+}
